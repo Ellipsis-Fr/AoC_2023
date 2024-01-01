fc69dcc802e99c0b3f2282aaacf70c4ecf651777
Ellipsis-Fr <crespel.romain@gmail.com>
2024-01-01 15:11:18 +0100

    init AoC_2023

---
+++ src/text_file_reader/mod.rs
@@ -0,0 +1,35 @@
+use std::env;
+
+use std::error::Error;
+use std::fs;
+
+const PATH_TO_PUZZLE: &str = "puzzles/";
+
+pub struct TextFileReader {
+    file_name: String,
+    content: Option<String>,
+}
+
+impl TextFileReader {
+    pub fn new(file_name: &str) -> TextFileReader {
+        TextFileReader {
+            file_name: file_name.to_string(),
+            content: None
+        }
+    }
+
+    pub fn read_file_text(&mut self) -> Result<(), Box<dyn Error>> {
+        let file_location = PATH_TO_PUZZLE.to_owned() + &self.file_name;
+        println!("localisation du fichier : {file_location}");
+        self.content = Some(fs::read_to_string(file_location)?);
+        Ok(())
+    }
+
+    pub fn get_content(&self) -> &str {
+        &self.content.as_ref().unwrap()
+    }
+
+    pub fn get_content_as_list_split_by_newline(&self) -> Vec<String> {
+        self.content.as_ref().unwrap().to_owned().lines().map(str::to_string).collect()
+    }
+}
