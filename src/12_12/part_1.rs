9b17c3dacf963f3ac660bee2eefb3b93b9bced4a
Ellipsis-Fr <crespel.romain@gmail.com>
2024-05-13 22:34:11 +0200

    in progress

--- src/12_12/part_1.rs
+++ src/12_12/part_1.rs
@@ -1,6 +1,10 @@
-use std::{cell::RefCell, rc::{Rc, Weak}};
+use std::{cell::RefCell, collections::VecDeque, rc::Rc};
 
 use AoC_2023::text_file_reader::TextFileReader;
+
+const OPERATIONAL_SPRING_SEARCH_REGEX: &str = r"(\s|\?)+";
+const DAMAGED_SPRING_SEARCH_REGEX: &str = r"(#|\?){n}";
+// voir comment concatener des regex, et y remplacer 'n' par le bon quantificateur
 
 #[derive(Debug, Clone)]
 enum SpringsState {
@@ -28,8 +32,8 @@ impl ArrangementSprings {
         ArrangementSprings { root: Rc::new(RefCell::new(Node::new(damaged_record_in_study, String::from("Racine")))) }
     }
 
-    fn search_for_existing_node(&self, by_damaged_record_in_study: Option<&str>, by_hypothesis: Option<&str>, level: u32) -> Vec<Rc<RefCell<Node>>> {
-        todo!()
+    fn search_for_existing_node(&self, by_damaged_record_in_study: Option<&str>, by_hypothesis: Option<&str>, level: u32) -> Option<Vec<Rc<RefCell<Node>>>> {
+        self.root.borrow().search_for_existing_node(by_damaged_record_in_study, by_hypothesis, level)
     }
 
 
@@ -49,9 +53,53 @@ impl Node {
         Node { /*parents: Vec::new(),*/ children: Vec::new(), damaged_record_in_study, hypothesis, number_of_arrangements_from_here: 0 }
     }
 
+    pub fn set_number_of_arrangements(&mut self) {
+        if self.children.is_empty() {
+            self.number_of_arrangements_from_here = 1;
+        } else {
+            self.number_of_arrangements_from_here += self.children.iter().map(|v| v.borrow().number_of_arrangements_from_here).sum::<i32>();
+        }
+    }
+
     pub fn add_children_nodes(&mut self, new_children: Vec<Rc<RefCell<Node>>>) {
-        self.number_of_arrangements_from_here += new_children.len() as i32;
         self.children.extend(new_children);
+    }
+
+    pub fn search_for_existing_node(&self, by_damaged_record_in_study: Option<&str>, by_hypothesis: Option<&str>, level: u32) -> Option<Vec<Rc<RefCell<Node>>>> {
+        let mut existing_nodes = Vec::new();
+
+        if level == 0 {
+            existing_nodes = self.children
+                .iter()
+                .filter(|child| {
+                    if by_damaged_record_in_study.is_some() {
+                        child.borrow().damaged_record_in_study == by_damaged_record_in_study.unwrap()
+                    } else {
+                        child.borrow().hypothesis == by_hypothesis.unwrap()
+                    }
+                })
+                .map(|child| Rc::clone(&child))
+                .collect::<Vec<_>>();
+        } else {
+            for child in &self.children {
+                match child.borrow().search_for_existing_node(by_damaged_record_in_study, by_hypothesis, level - 1) {
+                    Some(nodes) => {
+                        existing_nodes = nodes;
+                        break;
+                    },
+                    None => ()
+                }
+            }
+        }
+
+
+
+
+        if existing_nodes.is_empty() {
+            None
+        } else {
+            Some(existing_nodes)
+        }
     }
 }
 
@@ -77,18 +125,25 @@ fn count_possible_spring_arrangements(damaged_records: Vec<String>) -> i32 {
         let mut iter_damaged_record = damaged_record.split_whitespace();
         let (springs_line, sizes_of_group_of_damaged_springs) = (iter_damaged_record.next().unwrap().to_owned(), iter_damaged_record.next().unwrap().to_owned());
         let springs_in_unknown_state  = springs_line.split(".").filter(|v| !v.is_empty()).collect::<Vec<_>>();
-        let list_of_sizes_of_group_of_damaged_springs = sizes_of_group_of_damaged_springs.split(",").map(|v| v.parse::<i32>().unwrap()).collect::<Vec<_>>();
+        let list_of_sizes_of_group_of_damaged_springs = sizes_of_group_of_damaged_springs.split(",").map(|v| v.parse::<i32>().unwrap()).collect::<VecDeque<_>>();
         
         let mut arrangement_springs = ArrangementSprings::new(format!("{}+{}", springs_line, sizes_of_group_of_damaged_springs));
-        let all_children = search_arrangements(springs_in_unknown_state, list_of_sizes_of_group_of_damaged_springs, &arrangement_springs, Rc::clone(&arrangement_springs.root));
-
-
-        arrangement_springs.root.borrow_mut().add_children_nodes(all_children);
+        search_arrangements(
+            springs_in_unknown_state,
+            list_of_sizes_of_group_of_damaged_springs.clone(),
+            &arrangement_springs,
+            Rc::clone(&arrangement_springs.root),
+            0
+        );
         // let a = arrangment_springs.root;
         // println!("{:?}", a);
 
 
-        sum += arrangement_springs.root.borrow().number_of_arrangements_from_here;
+        sum += {
+            let mut root_borrowed_mut = arrangement_springs.root.borrow_mut();
+            root_borrowed_mut.set_number_of_arrangements();
+            root_borrowed_mut.number_of_arrangements_from_here
+        };
     }
     
     sum
@@ -96,11 +151,66 @@ fn count_possible_spring_arrangements(damaged_records: Vec<String>) -> i32 {
 
 fn search_arrangements(
     springs_in_unknown_state: Vec<&str>,
-    list_of_sizes_of_group_of_damaged_springs: Vec<i32>,
+    mut list_of_sizes_of_group_of_damaged_springs: VecDeque<i32>,
     arrangement_springs: &ArrangementSprings,
-    actual_node: Rc<RefCell<Node>>
-) -> Vec<Rc<RefCell<Node>>> {
+    rc_actual_node: Rc<RefCell<Node>>,
+    level: u32
+) -> bool {
+    let mut arrangement_found = false;
+
+    match arrangement_springs.search_for_existing_node(Some(&springs_in_unknown_state.join(",")), None, level) {
+        Some(existing_nodes) => {
+            arrangement_found = true;
+            let mut actuel_node_borrowed_mut = rc_actual_node.borrow_mut();
+            actuel_node_borrowed_mut.add_children_nodes(existing_nodes);
+            actuel_node_borrowed_mut.set_number_of_arrangements();
+        },
+        None => {
+            if let Some(size) = list_of_sizes_of_group_of_damaged_springs.pop_front() {
+                for (index, springs_group_in_unknown_state) in springs_in_unknown_state.iter().enumerate() {
+                    if springs_group_in_unknown_state.len() < size as usize {
+                        continue;
+                    }
+
+                    // TODO : Méthode contrôlant ce qu'il est possible de faire avec les entrées suivantes : 'springs_group_in_unknown_state' et 'size' 
+                    // TODO : et retournant un vec de tuple : [(arrangement réalisé pour ce sous groupe, reste de ce sous-groupe), ...], exemple : 
+                    // TODO : entrées : 'springs_group_in_unknown_state' = '???????' et 'size' = 2
+                    // TODO : sortie : [ ('##.', '????'), ('##.', '???'), ('##.', '??'), ('##.', '?'), ('##', '?'), ('##', '')] 
+                    // dans cet exemple les 2 dernières valeurs ne seont pas utilisé dû au contrôle de cohérence
+                    // TODO : Cette méthode procédera ainsi :
+                    // TODO : - contrôlera la cohérence de la demande
+                    // TODO : - parcourera la chaine 'springs_group_in_unknown_state' à partir de l'index 0 sur une suite de x valant 'size' et y associera le reste
 
 
-    todo!()
+
+
+                    // TODO : A partir de la précédente liste nous créons x noeud et y ajoutons à chaque fois le reste à la liste 'springs_in_unknown_state'
+                    // TODO : après y avoir substiuer le ou les 'springs_group_in_unknown_state' passé (cf. index)
+                    // TODO : Mais avant toute nouvelle relance on contrôle la cohérence du nouveau contenu de 'springs_in_unknown_state' avec la liste à jour de 'list_of_sizes_of_group_of_damaged_springs'
+
+
+
+
+    
+                    //// Donc finalement je gère la possible incohérence, via le retour bool de cette méthode appelée récursivement,
+                    //// quand 'false' me sera retourné je supprimerai ce noeud de la liste des enfants du noeud actuel
+
+                    // * Mais peut être qu'en utilisant une bonne regex je pourrais obtenir une solution me permettant de gérer les incohérences... car j'ai peur que ne pas les gérer cause des soucs de perte de temps...
+    
+                }
+            }
+        }
+    }
+
+    arrangement_found
+
+    //
+    // Nouveau fonctionnement :
+    // -dès que je crée un nouveau qui n'existe pas déjà je l'ajoute au noeud actuel
+    //- puis je lane à nouveau search arrangement
+    //- dès que je reviens au nouveau du noeud qui venait d'être créé et ajouté, je compte les arrangements du nouveau noeud
+    //
+    // ! Attention ce nouveau fonctionnement n'est possible que sous réserve que l'algo d'évaluation de cohérence soit infallible,
+    // ! sinon il faudra revoir ce fonctionnement et notamment y ajouter la possibilité qu'aucune correspondance ne soit trouvée
+    //
 }
